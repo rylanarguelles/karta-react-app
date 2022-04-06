f991eaf52245b8e93cd9e8d29d1a1ffab569a9ea
rylanarguelles <rylanarguelles@gmail.com>
2022-04-06 14:18:13 +0800

    Implement async/await for axios response

--- src/controller.js
+++ src/controller.js
@@ -2,9 +2,11 @@
 import AppService from "./services";
 
 export default class AppController {
-    static calculateBMI(unitSystem, height, weight) {
-        AppService.calculateBMI({ unitSystem, height, weight }).then((response) =>
-            console.log(response.data)
-        );
+    static async calculateBMI(unitSystem, height, weight) {
+        return await AppService.calculateBMI({
+            unitSystem,
+            height,
+            weight,
+        });
     }
 }
