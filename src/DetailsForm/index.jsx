e5a470291bdf777c2e00290ffde7e2565b749aec
rylanarguelles <rylanarguelles@gmail.com>
2022-04-06 10:21:15 +0800

    Add form button, onChange function

--- src/DetailsForm/index.jsx
+++ src/DetailsForm/index.jsx
@@ -1,22 +1,61 @@
 import React from "react";
-import { Button, Input, Space } from "antd";
+import { Button, InputNumber, Space } from "antd";
 
 export default class DetailsForm extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            height: null,
+            weight: null,
+        };
+
+        this.updateForm = this.updateForm.bind(this);
+        this.calculateBMI = this.calculateBMI.bind(this);
+    }
+
+    // Updates the appropriate detail in the state
+    updateForm = (property) => (e) => {
+        this.setState({
+            [property]: e,
+        });
+    };
+
+    // Triggers the BMI calculation to be done in the backend
+    calculateBMI() {
+        console.log(this.state);
+    }
+
     render() {
         const { isMetric } = this.props;
+        const incompleteForm =
+            this.state.height === null || this.state.weight === null;
         return (
             <Space direction="vertical">
-                <Input
+                <InputNumber
+                    type="number"
                     size="large"
                     addonAfter={isMetric ? "cm" : "ft. & inches"}
                     placeholder="Enter height"
+                    onChange={this.updateForm("height")}
                 />
                 <br />
-                <Input
+                <InputNumber
+                    type="number"
                     size="large"
                     addonAfter={isMetric ? "kg" : "lbs"}
                     placeholder="Enter weight"
+                    onChange={this.updateForm("weight")}
                 />
+                <br />
+                <Button
+                    onClick={this.calculateBMI}
+                    size="large"
+                    type="primary"
+                    disabled={incompleteForm}
+                >
+                    Calculate
+                </Button>
             </Space>
         );
     }
